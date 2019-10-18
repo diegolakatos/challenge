3eb56a5fbe56108b0b30658dcb2edc487f2a9e82
Diego Lakatos <diegovlakatos@gmail.com>
2019-10-18 08:55:41 -0300

    Add keypair creation and ec2 modules

--- main.tf
+++ main.tf
@@ -5,3 +5,15 @@ provider "aws" {
 module vpc {
   source = "./modules/vpc"
 }
+
+module keypair {
+  source = "./modules/keypair"
+}
+
+module elasticsearch {
+  source = "./modules/ec2"
+  vpc_id = "${module.vpc.vpc_id}"
+  key_pair = "${module.keypair.key_name}"
+  subnet_id = "${module.vpc.private_subnets}"
+  app_name = "elasticsearch"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,7 @@
+variable "number_of_instances" {
+  default = 3
+}
+
+variable "default_security_groups" {
+  type = "list"
+}
